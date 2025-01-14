d40c5c1f535db0268ababbe4416a68553de22be6
Lidor Borofker <155881749+Lidx7@users.noreply.github.com>
2025-01-14 19:20:06 +0200

    Update RaycastAttack.cs

--- Assets/Scripts/RaycastAttack.cs
+++ Assets/Scripts/RaycastAttack.cs
@@ -5,34 +5,42 @@ using UnityEngine.InputSystem;
 // from Fusion tutorial: https://doc.photonengine.com/fusion/current/tutorials/shared-mode-basics/5-remote-procedure-calls
 public class RaycastAttack : NetworkBehaviour {
     [SerializeField] int Damage;
+
     [SerializeField] InputAction attack;
     [SerializeField] InputAction attackLocation;
+
     [SerializeField] float shootDistance = 5f;
 
-    private void OnEnable() { attack.Enable(); attackLocation.Enable(); }
+    private void OnEnable() { attack.Enable(); attackLocation.Enable();  }
     private void OnDisable() { attack.Disable(); attackLocation.Disable(); }
-
     void OnValidate() {
         // Provide default bindings for the input actions. Based on answer by DMGregory: https://gamedev.stackexchange.com/a/205345/18261
-        if (attack == null) attack = new InputAction(type: InputActionType.Button);
-        if (attack.bindings.Count == 0) attack.AddBinding("<Mouse>/leftButton");
+        if (attack == null)
+            attack = new InputAction(type: InputActionType.Button);
+        if (attack.bindings.Count == 0)
+            attack.AddBinding("<Mouse>/leftButton");
 
-        if (attackLocation == null) attackLocation = new InputAction(type: InputActionType.Value, expectedControlType: "Vector2");
-        if (attackLocation.bindings.Count == 0) attackLocation.AddBinding("<Mouse>/position");
+        if (attackLocation == null)
+            attackLocation = new InputAction(type: InputActionType.Value, expectedControlType: "Vector2");
+        if (attackLocation.bindings.Count == 0)
+            attackLocation.AddBinding("<Mouse>/position");
     }
 
+
     private bool _attackPressed;
-    void Update() {
-        // We have to read the button status in Update, because FixedNetworkUpdate might miss it.
+    void Update() {  // We have to read the button status in Update, because FixedNetworkUpdate might miss it.
         if (!HasStateAuthority) return;
-        if (attack.WasPerformedThisFrame()) _attackPressed = true;
+        if (attack.WasPerformedThisFrame()) {
+            _attackPressed = true;
+        }
     }
 
     public override void FixedUpdateNetwork() {
-        if (!HasStateAuthority) return;
+        if (!HasStateAuthority)  return;
 
         if (_attackPressed) {
             Vector2 attackLocationInScreenCoordinates = attackLocation.ReadValue<Vector2>();
+
             var camera = Camera.main;
             Ray ray = camera.ScreenPointToRay(attackLocationInScreenCoordinates);
             ray.origin += camera.transform.forward;
@@ -41,7 +49,7 @@ public class RaycastAttack : NetworkBehaviour {
 
             if (Runner.GetPhysicsScene().Raycast(ray.origin, ray.direction * shootDistance, out var hit)) {
                 GameObject hitObject = hit.transform.gameObject;
-                Debug.Log("Raycast hit: name=" + hitObject.name + " tag=" + hitObject.tag + " collider=" + hit.collider);
+                Debug.Log("Raycast hit: name="+ hitObject.name+" tag="+hitObject.tag+" collider="+hit.collider);
                 if (hitObject.TryGetComponent<Health>(out var health)) {
                     Debug.Log("Dealing damage");
                     health.DealDamageRpc(Damage);
@@ -50,4 +58,6 @@ public class RaycastAttack : NetworkBehaviour {
             _attackPressed = false;
         }
     }
+
+
 }
